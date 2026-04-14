1b4e6f8a2775cacb70ff5c07250a46aa12cf941b
katherine-2007 <katherinemolinachilatra@gmail.com>
2026-04-14 13:31:59 -0500

    feat: agregar componente de prestamo - Katherine

---
+++ src/app/prestamos/infrastructure/prestamo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Prestamo } from '../domain/prestamo.model';
+import { PrestamoService } from '../application/prestamo.service';
+
+@Component({
+  selector: 'app-prestamo',
+  template: `
+    <h2>Prestamos</h2>
+    <ul>
+      <li *ngFor="let prestamo of prestamos">
+        {{ prestamo.id_prestamo }} - {{ prestamo.nombre_material }} - 
+        {{ prestamo.fecha_prestamo }} - {{ prestamo.estado }}
+      </li>
+    </ul>
+  `
+})
+export class PrestamoComponent implements OnInit {
+  prestamos: Prestamo[] = [];
+
+  constructor(private prestamoService: PrestamoService) {}
+
+  ngOnInit(): void {
+    this.prestamoService.getAll().subscribe({
+      next: (data) => this.prestamos = data,
+      error: (err) => console.error('Error al obtener prestamos', err)
+    });
+  }
+}
