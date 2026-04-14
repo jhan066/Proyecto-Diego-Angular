7d44617135cce5607756941ca9c6302a03a82817
katherine-2007 <katherinemolinachilatra@gmail.com>
2026-04-14 01:00:23 -0500

    feat: agregar componente de programa - Katherine

---
+++ src/app/programa/infrastructure/programa.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Programa } from '../domain/programa.model';
+import { ProgramaService } from '../application/programa.service';
+
+@Component({
+  selector: 'app-programa',
+  template: `
+    <h2>Programas</h2>
+    <ul>
+      <li *ngFor="let programa of programas">
+        {{ programa.codigo }} - {{ programa.nombre_programa }} - {{ programa.estado }}
+      </li>
+    </ul>
+  `
+})
+export class ProgramaComponent implements OnInit {
+  programas: Programa[] = [];
+
+  constructor(private programaService: ProgramaService) {}
+
+  ngOnInit(): void {
+    this.programaService.getAll().subscribe({
+      next: (data) => this.programas = data,
+      error: (err) => console.error('Error al obtener programas', err)
+    });
+  }
+}
